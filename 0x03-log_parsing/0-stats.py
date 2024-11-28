1b8dd566b5ee9935cefcdd5e19ac300faa2b40e8
Ben Obame <obameben@yahoo.fr>
2024-11-28 10:27:54 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,50 @@
 #!/usr/bin/python3
 """
-Reads stdin line by line and computes metrics.
+log parsing
 """
+
 import sys
-import signal
-
-file_size = 0
-status_codes = {}
-line_count = 0
+import re
 
 
-def print_stats():
-    """Prints the accumulated statistics."""
-    print(f"File size: {file_size}")
-    for status in sorted(status_codes.keys()):
-        print(f"{status}: {status_codes[status]}")
+def output(log: dict) -> None:
+    """
+    helper function to display stats
+    """
+    print("File size: {}".format(log["file_size"]))
+    for code in sorted(log["code_frequency"]):
+        if log["code_frequency"][code]:
+            print("{}: {}".format(code, log["code_frequency"][code]))
 
 
-def signal_handler(sig, frame):
-    """Handles the SIGINT signal (Ctrl+C)."""
-    print_stats()
-    sys.exit(0)
+if __name__ == "__main__":
+    regex = re.compile(
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')  # nopep8
 
+    line_count = 0
+    log = {}
+    log["file_size"] = 0
+    log["code_frequency"] = {
+        str(code): 0 for code in [
+            200, 301, 400, 401, 403, 404, 405, 500]}
 
-signal.signal(signal.SIGINT, signal_handler)
+    try:
+        for line in sys.stdin:
+            line = line.strip()
+            match = regex.fullmatch(line)
+            if (match):
+                line_count += 1
+                code = match.group(1)
+                file_size = int(match.group(2))
 
-try:
-    for line in sys.stdin:
-        try:
-            parts = line.split()
-            status_code = int(parts[-2])
-            file_size += int(parts[-1])
-            status_codes[status_code] = status_codes.get(status_code, 0) + 1
-        except (IndexError, ValueError):
-            pass  # Skip lines with invalid format
+                # File size
+                log["file_size"] += file_size
 
-        line_count += 1
-        if line_count % 10 == 0:
-            print_stats()
+                # status code
+                if (code.isdecimal()):
+                    log["code_frequency"][code] += 1
 
-except KeyboardInterrupt:
-    print_stats()
-
-print_stats()
+                if (line_count % 10 == 0):
+                    output(log)
+    finally:
+output(log)
