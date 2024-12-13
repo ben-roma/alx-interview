af8c9f5839785bb809909871397dae0eb590df06
Ben Obame <obameben@yahoo.fr>
2024-12-13 08:23:10 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def is_prime(n):
-    """Efficiently checks if a number is prime."""
+    """Check if a number is prime."""
     if n < 2:
         return False
     for i in range(2, int(n**0.5) + 1):
@@ -9,47 +9,49 @@ def is_prime(n):
             return False
     return True
 
-def get_primes(n):
-    """Generates a list of primes up to n using the Sieve of Eratosthenes."""
-    primes = []
-    is_prime_list = [True] * (n + 1)
-    for i in range(2, n + 1):
-        if is_prime_list[i]:
-            primes.append(i)
-            for j in range(i * i, n + 1, i):
-                is_prime_list[j] = False
-    return primes
+def get_winner(n):
+    """Determine the winner for a single game up to n."""
+    if n < 1:
+        return 0
+
+    nums = list(range(1, n + 1))
+    maria_turn = True
+    moves = 0
+
+    while True:
+        possible_moves = []
+        for num in nums:
+            if is_prime(num):
+                possible_moves.append(num)
+
+        if not possible_moves:
+            break
+
+        chosen_prime = possible_moves[0]  # Optimal play: choose the 
+        nums = [num for num in nums if num % chosen_prime != 0]
+        moves += 1
+        maria_turn = not maria_turn
+
+    if moves % 2 == 1:  # Odd number of moves means Maria won
+        return 1
+    else:
+        return 0
 
 def isWinner(x, nums):
-    """Determines the winner of the Prime Game."""
+    """Determine the overall winner across multiple games."""
     if not nums or x <= 0:
         return None
-
-    max_n = max(nums) if nums else 0
-    primes = get_primes(max_n)
-    prime_counts = [0] * (max_n + 1)
-
-    for i in range(1, max_n + 1):
-        count = 0
-        for p in primes:
-            if p > i:
-                break
-            if i % p == 0:
-                count += 1
-        prime_counts[i] = count
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if n == 0 or n == 1:
-            ben_wins += 1
-            continue
-        if prime_counts[n] % 2 == 0:
-            ben_wins += 1
-        else:
+        winner = get_winner(n)
+        if winner == 1:
             maria_wins += 1
-            
+        else:
+            ben_wins += 1
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
